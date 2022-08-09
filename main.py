426ee82fc5b2ece44bf3e8b439123be8993c721b
likhith.jalimedi@gmail.com <likhith.jalimedi@gmail.com>
2022-08-09 12:42:30 +0530

    Added Bye endpoint

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {'message' : "Hello World"}
+    return {'message': "Hello World"}
 
 
 @app.get("/greet/{name}")
 async def root(name):
-    msg = "Hello "+ name
+    msg = "Hello " + name
     return {'greetings': msg}
 
+
+@app.get("/bye/{name}")
+async def say_bye(name):
+    bye_msg = "Bye " + name
+    return {'greet': bye_msg}
